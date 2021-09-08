d8a627bd4f701744940c513f1300be75a17360f5
배형준 <sunnyjun01@gmail.com>
2021-09-08 10:35:52 +0000

    1

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,22 @@
+#include <iostream>
 #include <stdio.h>
+
+using namespace std;
 
 int main()
 {
-	printf("a");
+	FILE* pFile = fopen("resource/sample.jpg", "r"); //read mode 
+    if(pFile == NULL)
+    {
+        printf("파일이 없습니다. 프로그램을 종료합니다.");
+        return 0;
+    }
+	
+	char str[50];
+    fgets(str, 50, pFile);  //파일에 있는거 읽어오기
+    
+    fclose(pFile);          //파일 닫기
+    printf("%s\n", str);    //터미널에 출력
+
 	return 0;
 }
