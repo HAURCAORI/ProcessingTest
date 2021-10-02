ef16f984444e2dd4a803fdcb1dcd36b327a1f953
Guest <guest-7cjiah@h25033.sgb.skir.kr>
2021-10-02 21:06:16 +0900

    vscode

--- include/Mapping.h
+++ include/Mapping.h
@@ -5,7 +5,7 @@
 using namespace std;
 
 bool Mapping();
-
+void ErrorMsg(bool type, string code, int index, string message);
 // SYNTAX
 // [group1]
 // n1[12.4,12.0],mode,n2/n3/n4/n5/n6/1321;12312/group2[partial]
@@ -14,7 +14,7 @@ bool Mapping();
 // n1[1.0,2.0],mode,n1/group1;n2
 
 // 첫번째 항 : neuron의 id, 임의 명칭 지정 가능 괄호안 [threshold, weight], 괄호는 생략 가능, 그룹별로 id는 별개로 취급
-// 두번째 항 : 주소 등록 방식(공백은 뒤에 있는 주소를 이용, 'z'는 주소 없이 다음 neuron으로 전달, 'i'는 input neuron으로 사용, 'o'은 output peuron으로 사용)
+// 두번째 항 : 주소 등록 방식(공백은 뒤에 있는 주소를 이용, 'x'는 주소 없이 다음 neuron으로 전달, 'i'는 input neuron으로 사용, 'o'은 output peuron으로 사용)
 // 세번째 항 : 주소 목록(형식 : neuron id 또는 page;sector 또는 group)
 
 
--- src/Mapping.cpp
+++ src/Mapping.cpp
@@ -11,7 +11,6 @@ class Group
 {
 	private:
 	string name;
-	streampos pos;
 	int count;
 	vector<string> vector_string;
 	public:
@@ -19,10 +18,9 @@ class Group
 	{
 		count = 0;
 	}
-	Group(string name, streampos pos)
+	Group(string name)
 	{
 		this->name = name;
-		this->pos = pos;
 		count = 0;
 	}
 
@@ -48,14 +46,16 @@ struct neuronTag
 	int estimate;
 	vector<string> stream;
 	PAGE page;
-	SECTOR sector;
-	vector<BYTE> bytes;
+	SECTOR sector = USHORT_MAX;
+	vector<BYTES> *bytes;
 };
 
 void Processing();
 vector<Group> vector_group;
-vector<string> group_name_list;
+vector<string> group_name_list;// 빠른 검색을 위한 그룹 이름 지정
 vector<neuronTag> neuron_list;
+const vector<string> methods = {"", "x","i","o"};
+
 
 string error;
 
@@ -66,21 +66,29 @@ bool Mapping(){
     {
 		string str;
 		
-		streampos pos; // 파일에서의 위치
 		int line = 1;
 		size_t index; // 인덱스 검색 시 사용
 		Group group;
 		string current_group;
 		bool previous = false;
+		bool valid = false;
+		bool end = false;
+		//----------------------//
+		//그룹 확인 및 그룹 데이터 지정
+		//----------------------//
         while (!file.eof())
         {
-			pos = file.tellg();
+			//pos = file.tellg();
 			getline(file, str);
+
+			//==========//
+			//#태그 확인부
+			//==========//
 			if(str.front() == '#')
 			{
-				transform(str.begin(), str.end(),str.begin(), ::toupper);
-				if(str == "#BEGIN")
-				{
+				transform(str.begin(), str.end(),str.begin(), ::toupper);//대문자로 치환
+				if(str == "#BEGIN"){
+					valid = true;
 					continue;
 				}
 				else if (str == "#END")
@@ -88,50 +96,76 @@ bool Mapping(){
 					if(previous == true)
 					{
 						vector_group.push_back(group);
+						end = true;
 					}
 					break;
 				}
 			}
-
-			if(str.front() == ' ' || str.empty())
+			if(valid == true)
 			{
-				continue;
-			}
-			else if(str.front() == '[') //group
-			{
-				if(previous == true)
+				//==========//
+				//line처리부
+				//==========//
+				if(str.front() == ' ' || str.empty()) //공백 무시
 				{
-					vector_group.push_back(group);
+					continue;
 				}
-
-				index = str.find_last_of(']');
-				if(index == string::npos)
+				else if(str.front() == '[') //Group여부 확인
 				{
-					error.append("[ERROR] Can't find ']'\n");
-				}
-				current_group = str.substr(1,index-1);
-				group_name_list.push_back(current_group);
-				group = Group(current_group,pos);
-				previous = true;
-				continue;
-			}
-			else if(previous == true)
-			{
-				group.AddItem(str);
-			}
+					if(previous == true)
+					{
+						vector_group.push_back(group);
+					}
 
-			line++;
+					index = str.find_last_of(']'); //대괄호 종결부 확인
+					if(index == string::npos)
+					{
+						ErrorMsg(true,"GROUP", line, "Can't find ']'.");
+						valid = false;
+					}
+					current_group = str.substr(1,index-1);
+					group_name_list.push_back(current_group);
+					group = Group(current_group);
+					previous = true;
+					continue;
+				}
+				else if(previous == true)
+				{
+					group.AddItem(str);//Group내의 모든 데이터행을 추가
+				}
+				line++;
+			}
 		}
 		file.close();
-		//cout << "size : " << vector_group.size() << endl;
-		//cout << vector_group[1].getCount() << endl;
-		Processing();
+		
+		if(end == false)
+		{
+			ErrorMsg(false,"FILE", 0, "Can't find '#END'.");
+		}
+
+		if(valid == true)
+		{
+			Processing(); //그룹화가 끝나면 프로세싱 진행
+		}
+		else
+		{
+			if(previous == false)
+				ErrorMsg(true,"FILE", 0, "Can't find '#BEGIN'.");
+			ErrorMsg(true,"FILE", 0, "Failed to mapping.");
+		}
+	}else{
+		ErrorMsg(true,"FILE", 0, "Can't open 'mapping' file.");
+		cout << error << endl;
+		return false;
 	}
 	cout << error << endl;
     return true;
 }
 
-int elementCount(string group){//최적화 위해서 나중에는 변수로 이식해두기
+//++++++++++//
+//함수 정의부
+//++++++++++//
+int elementCount(string group){ // 해당 그룹의 요소 개수 반환(필요시 나중에 변수로 저장하도록)
 	int count = 0;
 	for(size_t i = 0; i < neuron_list.size(); i++){
 		if(neuron_list[i].group == group)
@@ -152,11 +186,29 @@ bool inGroup(string name)
 	return false;
 }
 
+bool inMethod(string method) 
+{
+	for(size_t i = 0; i < methods.size(); i++)
+	{
+		if(methods[i] == method)
+			return true;
+	}
+	return false;
+}
+//----------//
+//함수 정의부
+//----------//
+
+//********************//
+//  Processing()
+//********************//
 void Processing(){
-	//1차원 neuron 목록 생성
+	//----------------------//
+	// i. 1차원 neuron 목록 생성
+	//----------------------//
 	int n = vector_group.size();
 	size_t index;
-	for(int i = 0; i < n; i++)
+	for(int i = 0; i < n; i++)//그룹의 항목 검색
 	{
 		for(int j = 0; j < vector_group[i].getCount(); j++)
 		{
@@ -171,6 +223,16 @@ void Processing(){
 					neuron_id = div[0];
 				}else{
 					neuron_id = div[0].substr(0,index);
+					if(div[0].find_last_of(']') == string::npos)
+					{
+						ErrorMsg(false,vector_group[i].getName(), j+1, "Inappropriate ID Property. Can't find ']'.");
+						continue;
+					}
+				}
+				if(!inMethod(div[1]))
+				{
+					ErrorMsg(false,vector_group[i].getName(), j+1, "There is no method named '" + div[1] + "'.");
+					continue;
 				}
 				neuronTag tag;
 				tag.id = neuron_id;
@@ -178,82 +240,171 @@ void Processing(){
 				tag.stream = div;
 				neuron_list.push_back(tag);
 			}else{
-				error.append("[WARNING] ");
-				error.append(vector_group[i].getName());
-				error.append("|Line:");
-				error.append(to_string(j+1));
-				error.append(" :: Inappropriate Count of Parameters\n");
+				ErrorMsg(false,vector_group[i].getName(), j+1, "Inappropriate Count of Parameters.");
 			}
 		}
 	}
-
-	// 주소 개수 판단
+	//----------------------//
+	// ii. 주소 개수 판단
+	//----------------------//
 	for(size_t i = 0; i < neuron_list.size(); i++)
 	{
-		int estimate = 18; //종결자 2bytes + header 16bytes
-		if(neuron_list[i].stream[1].empty())
+		int estimate = 18; //종결자 2bytesp[0xff 0xff] + header 16bytes
+		if(neuron_list[i].stream[1].empty()) //***기본 지정자***
 		{
 			vector<string> div = split(neuron_list[i].stream[2],'/');
 			for(size_t n = 0; n < div.size(); n++)
 			{
-				if(div[n].find(';') != string::npos)
+				if(div[n].find(';') != string::npos) //그룹여부는 ';' 유무로 구분
 				{
 					index = div[n].find_first_of(';');
 					string first = div[n].substr(0,index);
 					string second = div[n].substr(index+1);
-					if(isnumber(first) && isnumber(second)){
-						estimate += 4;
+					if(isnumber(first) && isnumber(second)){ //숫자쌍은 단일 주소 의미
+						estimate += 6; //전환자 + 해당Page 주소 + 단일 데이터 sector 주소
 					}else{
 						if(inGroup(first))
 						{
 							if(second.empty())
 							{
-								estimate += elementCount(first)*2;
-								estimate += 2; //전환자
+								//해당 그룹의 모든 데이터로 연결
+								estimate += elementCount(first)*2; 
+								estimate += 4; //전환자 + 해당Page 주소
 							}else{
-								size_t c = std::count(second.begin(), second.end(), '|')+1;
+								//해당 그룹의 특수 데이터로 연결
+								size_t c = std::count(second.begin(), second.end(), '|')+1;// |로 주소 구분
 								estimate += c*2;
-								estimate += 2; //전환자
+								estimate += 4; //전환자 + 해당Page 주소
 							}
 						}else{
-							error.append("[ERROR] [");
-							error.append(neuron_list[i].group);
-							error.append("] :: Can't Find group name '");
-							error.append(first);
-							error.append("'\n");
+							string err = first;
+							estimate = 0;
+							ErrorMsg(false,neuron_list[i].group, i+1, "Can't find group name '" + err + "'.");
+							break;
 						}
 					}
 				}else{
-					estimate += 2;
+					estimate += 2; //기본 주소로 생각
 				}
 			}
+		}else if(neuron_list[i].stream[1] == "x") //***next 지정자***
+		{
+			if((i+1) == neuron_list.size())
+			{
+				estimate = 0;
+				ErrorMsg(false,neuron_list[i].group, i+1, "Can't find next neuron.");
+			}else{
+				estimate = 16;
+			}
+			
 		}
-
 		neuron_list[i].estimate = estimate;
 	}
+	//문법 오류가 있는 데이터 삭제
+	for(vector<neuronTag>::size_type i=0; i < neuron_list.size();){ 
+		if(neuron_list[i].estimate == 0){
+			neuron_list.erase(neuron_list.begin() + i);
+		}else
+			i++;
+	}
 
-	// 각 뉴런의 주소 할당
+	//----------------------//
+	// iii. 각 뉴런의 주소 할당
+	//----------------------//
 	int page = 0;
 	int sector = 0;
+	int countpage = 0;
+	int countsector = 0;
 	for(size_t i = 0; i < neuron_list.size(); i++)
 	{
-		int n = (neuron_list[i].estimate / SectorUnit) + 1;
-		if((sector + n > USHORT_MAX-1)) //65535 주소는 사용할 수 없는 주소니 할당되는 것 방지
+		int num = (neuron_list[i].estimate / SectorUnit) + 1;
+		if(neuron_list[i].estimate == 0)
 		{
-			page += 1;
-			sector = 0;
+			continue;
 		}
+		else if(neuron_list[i].estimate == 16)
+		{
+			int nn = (neuron_list[i+1].estimate / SectorUnit) + 1;//다음 뉴런의 정보도 고려
+			if((sector + nn > USHORT_MAX-1))
+			{
+				page += 1;
+				sector = 0;
+				++countpage;
+			}
+			neuron_list[i].page = page;
+			neuron_list[i].sector = sector;
+			
+			++sector;
+			++countsector;;
+		}
+		else
+		{
+			if((sector + num > USHORT_MAX-1)) //65535 주소는 사용할 수 없는 주소니 할당되는 것 방지
+			{
+				page += 1;
+				sector = 0;
+				++countpage;
+			}
+			neuron_list[i].page = page;
+			neuron_list[i].sector = sector;
 
-		neuron_list[i].page = page;
-		neuron_list[i].sector = sector;
-
-		sector += n;
+			sector += num;
+			countsector += num;
+		}
+		cout << neuron_list[i].id << "(" << num << ") :" << neuron_list[i].page << "/" << neuron_list[i].sector << endl;
 		
-		//cout << neuron_list[i].id << "(" << n <<  ") :" << neuron_list[i].page << "/" << neuron_list[i].sector << endl;
+	}
+
+	//----------------------//
+	// iv. tag에 주소 기록
+	//----------------------//
+	for(size_t i = 0; i < neuron_list.size(); i++)
+	{
+		vector<BYTES> temp;
+		if(neuron_list[i].estimate==0)
+		{
+			continue;
+		}
+		else if(neuron_list[i].stream[1].empty()) //***기본 지정자***
+		{
+			vector<string> div = split(neuron_list[i].stream[2],'/');
+			for(size_t n = 0; n < div.size(); n++)
+			{
+				if(div[n].find(';') != string::npos) //그룹여부는 ';' 유무로 구분
+				{
+					index = div[n].find_first_of(';');
+					string first = div[n].substr(0,index);
+					string second = div[n].substr(index+1);
+					if(isnumber(first) && isnumber(second)){ //숫자쌍은 단일 주소 의미
+						
+					}else{
+						if(inGroup(first))
+						{
+							if(second.empty())
+							{
+								//해당 그룹의 모든 데이터로 연결
+								
+							}else{
+								//해당 그룹의 특수 데이터로 연결
+								
+							}
+						}else{
+							ErrorMsg(false,neuron_list[i].group, i+1, "EEEEEEEEEE"); //옳게 되면 나오면 안되는 오류
+						}
+					}
+				}else{
+					//기본 주소로 처리
+				}
+			}
+		}else if(neuron_list[i].stream[1] == "x") //***next 지정자***
+		{
+			
+		}
+		neuron_list[i].bytes = &temp;
 	}
 	
-	// 파일에 직접 기록
-	
+
+
 
 	/*
 	for(size_t i = 0; i < neuron_list.size(); i++)
@@ -261,4 +412,27 @@ void Processing(){
 		cout << neuron_list[i].group << "/" << neuron_list[i].id << endl;
 	}
 	*/
+}
+
+void ErrorMsg(bool type, string code, int index, string message)
+{
+	if(type == true)
+	{
+		error.append("[ERROR] ");
+		error.append(code);
+		error.append(" | Index:");
+		error.append(to_string(index));
+		error.append(" :: ");
+		error.append(message);
+		error.append("\n");
+	}
+	else{
+		error.append("[WARNING] ");
+		error.append(code);
+		error.append(" | Index:");
+		error.append(to_string(index));
+		error.append(" :: ");
+		error.append(message);
+		error.append("\n");
+	}
 }
